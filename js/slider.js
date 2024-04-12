59b189295c2cc07c7429bc15d47bfa29bf75f154
SoumyadebBasu <55205664+SoumyadebBasu@users.noreply.github.com>
2024-04-12 18:51:28 +0530

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".clients").slick({slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3,arrows:!1,focusOnSelect:!0,centerMode:!0,mobileFirst:!0,responsive:[{breakpoint:1100,settings:{slidesToShow:3,slidesToScroll:1}},{breakpoint:768,settings:{slidesToShow:2,slidesToScroll:1}}]})}));
